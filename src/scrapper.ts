fb0aa584c1b655aa83687b4ffd2dbfa991027b4b
Bourichi-Taha <bourichi.taha@gmail.com>
2025-06-23 10:30:38 +0100

    fixed typo

--- src/scrapper.ts
+++ src/scrapper.ts
@@ -48,7 +48,7 @@ export async function fetchDev(
       if (
         res.data?.message &&
         res.data?.message !== "App not found (404)" &&
-        (res.data?.message as string).includes("fantasy-land/map")
+        !(res.data?.message as string).includes("fantasy-land/map")
       ) {
         throw new Error(res.data.message);
       }
